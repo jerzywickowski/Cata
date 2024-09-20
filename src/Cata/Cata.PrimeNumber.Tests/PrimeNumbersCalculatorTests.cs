12feb2870a6a0721c824205b03804253e68bf6c8
Jerzy Wickowski <jerzy.wickowski@pracuj.pl>
2024-09-20 11:02:45 +0200

    add test for 2

---
+++ src/Cata/Cata.PrimeNumber.Tests/PrimeNumbersCalculatorTests.cs
@@ -0,0 +1,14 @@
+namespace Cata.PrimeNumber.Tests
+{
+    public class PrimeNumbersCalculatorTests
+    {
+        [Fact]
+        public void Calcualte_for_2()
+        {
+            var numbersCalculator = new PrimeNumbersCalculator();
+            int[] result = numbersCalculator.Calculate(2);
+            Assert.Equal(1, result.Length);
+            Assert.Equal(2, result[0]);
+        }
+    }
+}
