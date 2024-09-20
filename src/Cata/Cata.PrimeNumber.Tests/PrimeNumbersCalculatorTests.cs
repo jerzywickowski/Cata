4b8732fc10bbbc5f37e416a0c418b7dd88619ca7
Jerzy Wickowski <jerzy.wickowski@pracuj.pl>
2024-09-20 11:44:22 +0200

    test rafactor

--- src/Cata/Cata.PrimeNumber.Tests/PrimeNumbersCalculatorTests.cs
+++ src/Cata/Cata.PrimeNumber.Tests/PrimeNumbersCalculatorTests.cs
@@ -5,31 +5,16 @@ namespace Cata.PrimeNumber.Tests
 {
     public class PrimeNumbersCalculatorTests
     {
-        [Fact]
-        public void Calculate_for_2()
+        [Theory]
+        [InlineData(2, new [] { 2 })]
+        [InlineData(3, new [] { 3 })]
+        [InlineData(4, new [] { 2, 2 })]
+        public void Calculate_for_2(int input, int[] output)
         {
             var numbersCalculator = new PrimeNumbersCalculator();
-            int[] result = numbersCalculator.Calculate(2);
-            Assert.Equal(1, result.Length);
-            Assert.Equal(2, result[0]);
-        }
-        [Fact]
-        public void Calculate_for_3()
-        {
-            var numbersCalculator = new PrimeNumbersCalculator();
-            int[] result = numbersCalculator.Calculate(3);
-            Assert.Equal(1, result.Length);
-            Assert.Equal(3, result[0]);
-        }
-
-        [Fact]
-        public void Calculate_for_4()
-        {
-            var numbersCalculator = new PrimeNumbersCalculator();
-            int[] result = numbersCalculator.Calculate(4);
-            Assert.Equal(2, result.Length);
-            Assert.Equal(2, result[0]);
-            Assert.Equal(2, result[1]);
+            int[] result = numbersCalculator.Calculate(input);
+            Assert.Equal(output.Length, result.Length);
+            Assert.Equal(output, result);
         }
     }
 }
