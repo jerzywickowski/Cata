b9a5009d60dabd155a18439fa079a28b3e36bc82
Maciej Rożek <maciej.rozek@pracuj.pl>
2024-09-20 11:41:01 +0200

    pass test

--- src/Cata/Cata.PrimeNumbers/PrimeNumbersCalculator.cs
+++ src/Cata/Cata.PrimeNumbers/PrimeNumbersCalculator.cs
@@ -4,6 +4,19 @@
     {
         public int[] Calculate(int number)
         {
+            if (number % 2 == 0)
+            {
+                number = (number / 2);
+                if (number % 2 == 0)
+                {
+                    return new int[] { 2, 2 };
+                }
+                else
+                {
+                    return new int[] { 2 };
+                }
+            }
+
             return new int[] { number };
         }
     }
